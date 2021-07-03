8de0de38d32e97539d0dfa29465c7291008727fa
MasterMindAlex01 <alex.rodriguez@misena.edu.co>
2021-07-03 12:04:09 -0500

    docs add

---
+++ docs/precache-manifest.d53ed25496efad195522f0f515cda948.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d128ffaacefdfebe407570c85109aaad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f825a0cbc1d69c51635a",
+    "url": "/static/css/main.a0c3b307.chunk.css"
+  },
+  {
+    "revision": "21128ddbf1ff63946978",
+    "url": "/static/js/2.31a10122.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.31a10122.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f825a0cbc1d69c51635a",
+    "url": "/static/js/main.8373acf5.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
